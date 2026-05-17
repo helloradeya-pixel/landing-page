59277d8c8dc141c8c962ee0f928ceaaccda5f9b3
radeya <hello.radeya@gmail.com>
2026-05-18 04:15:40 +0700

    Update LeadForm.tsx

--- src/templates/LeadForm.tsx
+++ src/templates/LeadForm.tsx
@@ -187,15 +187,7 @@ Boleh dibantu info detail paketnya ya 🙏`;
               }}
             />
           </div>
-
-          <p className="text-xs text-neutral-500">
-
-    Estimasi tanggal (opsional kalau belum fix)
-
-  </p>
-
-</div>
-
+          
           <select
             name="budget"
             value={form.budget}
